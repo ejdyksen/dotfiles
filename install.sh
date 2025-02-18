1124bd6e8f665fda4ab4ccb91388c1578797b9b2
E.J. Dyksen <ej@dyksen.net>
2025-02-18 18:50:23 -0500

    More robust installation

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Check if SCRIPT_PATH is not under HOME, create .dotfiles symlink if needed
+if [[ "$SCRIPT_PATH" != "$HOME"* ]]; then
+  echo "Dotfiles not in HOME directory, creating .dotfiles symlink"
+  if [[ -L "$HOME/.dotfiles" ]]; then
+    rm "$HOME/.dotfiles"
+  fi
+  ln -s "$SCRIPT_PATH" "$HOME/.dotfiles"
+fi
+
 if [[ $1 == "-f" ]]; then
   FORCE=1
 else
@@ -9,32 +18,44 @@ else
 fi
 
 function linkdotfile {
-  SRC="$SCRIPT_PATH/$1"
-  DEST="$HOME/$1"
+  # Support both direct paths and paths with source/dest format
+  if [[ $# -eq 2 ]]; then
+    SRC="$SCRIPT_PATH/$1"
+    DEST="$HOME/$2"
+  else
+    SRC="$SCRIPT_PATH/$1"
+    DEST="$HOME/$1"
+  fi
+
+  # Create parent directory if it doesn't exist
+  DEST_DIR=$(dirname "$DEST")
+  if [[ ! -d "$DEST_DIR" ]]; then
+    mkdir -p "$DEST_DIR"
+  fi
 
   # Only delete dirs if -f
   if [[ $FORCE -eq 1 && -d $DEST ]]; then
     echo "Linking: $DEST (forced)"
-    rm -rf $DEST
-    ln -s $SRC $DEST
+    rm -rf "$DEST"
+    ln -s "$SRC" "$DEST"
     return
   fi
 
-  if [[ -d $DEST ]]; then
+  if [[ -d $DEST && ! -L $DEST ]]; then
     echo "Skipping: $DEST (dir exists)"
     return
   fi
 
   # Just always delete files/links
-  if [[ -f $DEST || -L $DEST  ]]; then
-    rm -f $DEST
+  if [[ -f $DEST || -L $DEST ]]; then
+    rm -f "$DEST"
   fi
 
   echo "Linking: $DEST"
-  ln -s $SRC $DEST
+  ln -s "$SRC" "$DEST"
 }
 
-linkdotfile .zshenv
+linkdotfile "zsh/.zshenv" ".zshenv"
 
 linkdotfile .gitconfig
 linkdotfile .gitignore_global
