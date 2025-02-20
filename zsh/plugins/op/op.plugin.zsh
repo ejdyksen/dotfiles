4f3481cd341251a73891af4cf20b3bd004f0f1d2
E.J. Dyksen <ej@dyksen.net>
2025-02-19 23:34:02 -0500

    Fix 1Password and aws interaction

--- zsh/plugins/op/op.plugin.zsh
+++ zsh/plugins/op/op.plugin.zsh
@@ -1,5 +1,9 @@
 (( ${+commands[op]} )) || return
 
-source ~/.config/op/plugins.sh
+# Do this by hand instead of using the plugin system that 1Passowrd has
+# because it uses aliases which break completion.
+
+export OP_PLUGIN_ALIASES_SOURCED=1
+aws() { op plugin run -- aws "$@" }
 
 eval "$(op completion zsh)"; compdef _op op
