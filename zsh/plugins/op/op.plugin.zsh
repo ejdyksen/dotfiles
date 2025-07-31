eff7e2d402563ef171d0697c00f4ad2aa17cf5d1
E.J. Dyksen <ej@dyksen.net>
2025-07-30 23:44:39 -0400

    Disable 1Password AWS plugin

--- zsh/plugins/op/op.plugin.zsh
+++ zsh/plugins/op/op.plugin.zsh
@@ -4,6 +4,8 @@
 # because it uses aliases which break completion.
 
 export OP_PLUGIN_ALIASES_SOURCED=1
-aws() { op plugin run -- aws "$@" }
+
+# Disable op aws plugin in favor of AWS SSO
+# aws() { op plugin run -- aws "$@" }
 
 eval "$(op completion zsh)"; compdef _op op
