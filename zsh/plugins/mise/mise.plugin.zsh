0617f8dca0d02606cf0b3224223056aef5beb52a
E.J. Dyksen <ej@dyksen.net>
2025-08-10 09:38:18 -0500

    Clarification on mise plugin

--- zsh/plugins/mise/mise.plugin.zsh
+++ zsh/plugins/mise/mise.plugin.zsh
@@ -1,3 +1,5 @@
 if (( ${+commands[mise]} )); then
+  # This overrides the `mise activate zsh --shims` command in .zprofile for interactive shells
+  # This puts actual paths to real binaries on the PATH, instead of shims
   eval "$(mise activate zsh)"
 fi
